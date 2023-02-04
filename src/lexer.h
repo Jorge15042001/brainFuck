2f0481eb5e0fbb39b3f40d3af6c9d789e7bea2fa
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-02-04 13:05:39 -0500

    fix types and format

--- src/lexer.h
+++ src/lexer.h
@@ -1,13 +1,14 @@
 #pragma once
 
 
+#include <stddef.h>
 typedef struct{
   int error;// 1 si hubo algun error, O si no hubo errores
-  int*tokens;// list of tokens
-  unsigned int length;// numero de tokens encontrados
+  int* tokens;// list of tokens
+  size_t length;// numero de tokens encontrados
 }LexerResult ;
 
-LexerResult RunLexicalAnalisys(const char *buffer,const int length);
+LexerResult RunLexicalAnalisys(const char *buffer,const size_t length);
 
 void printLexerResult(const LexerResult* const r);
 
