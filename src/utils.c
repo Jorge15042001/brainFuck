1e614d92302f9cc66af6c0c88133b9a184b1099a
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-02-04 13:04:43 -0500

    fix

--- src/utils.c
+++ src/utils.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 
-char *inputString(){
+char *inputString(void){
 //The size is extended by the input with the value of the provisional
     FILE * fp = stdin;
     char *str;
@@ -14,7 +14,7 @@ char *inputString(){
     str = realloc(NULL, sizeof(*str)*size);//size is start size
     if(!str)return str;
     while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
-        str[len++]=ch;
+        str[len++]=(char)ch;
         if(len==size){
             str = realloc(str, sizeof(*str)*(size+=16));
             if(!str)return str;
