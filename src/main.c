c4986c7296a2b3e13cceac0ebde22d8f7e8de6bc
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-02-04 13:16:22 -0500

    change prints and allow the user to quit

--- src/main.c
+++ src/main.c
@@ -8,19 +8,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 int main(void){
 
-  printf("Brainkfuck interpreter");
+  printf("Brainkfuck interpreter\n");
 
   while (1) {
 
-    printf("\nIngrese un sting de brainfuck a ser analizado: ");
+    printf(">>");
     //leer una cadena de consola y pasarla por el analizador sintactico
     
     char * const brainfuck_src = inputString();
+    if ( strcmp (brainfuck_src,"quit")==0)break;
     const LexerResult r = RunLexicalAnalisys(brainfuck_src, strlen(brainfuck_src));
 
     // create an interpreter
